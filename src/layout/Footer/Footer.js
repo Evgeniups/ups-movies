0904280e55dd8af7627b455964dcd2f5cf944022
Eugene Skvortsov <evgeniups@gmail.com>
2023-12-26 14:04:06 +0200

    vercel

--- src/layout/Footer/Footer.js
+++ src/layout/Footer/Footer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 function Footer(props) {
-	return (
-		<footer className="page-footer teal lighten-2 ">
-			<div className="footer-copyright">
-				<div className="container">
-					© {new Date().getFullYear()} Copyright Text
-					<a className="grey-text text-lighten-4 right" href="#!">Repo</a>
-				</div>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className='page-footer teal lighten-2 '>
+      <div className='footer-copyright'>
+        <div className='container'>
+          © {new Date().getFullYear()} All rights reserved
+          <a className='grey-text text-lighten-4 right' href='https://evgeniups.space/'>
+            Portfolio
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,20 +1,20 @@
 import React from 'react';
 
 const Header = () => {
-	return (
-		<nav>
-			<div className="nav-wrapper teal ">
-				<a href="https://evgeniups.github.io/ups-movies" className="brand-logo ">
-					React movies
-				</a>
-				<ul id="nav-mobile" className="right hide-on-med-and-down">
-					<li>
-						<a href="https://github.com/EvgeniUPS/ups-movies">Repo</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
+  return (
+    <nav>
+      <div className='nav-wrapper  cyan darken-4 '>
+        <a href='http://movies.evgeniups.space/' className='brand-logo '>
+          React movies
+        </a>
+        <ul id='nav-mobile' className='right hide-on-med-and-down'>
+          <li>
+            <a href='https://evgeniups.space/'>Portfolio</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
